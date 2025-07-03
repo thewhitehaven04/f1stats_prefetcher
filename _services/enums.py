8e35480123fcfca8c9806f7938a30c46c15760d3
German <thewhitehaven04@gmail.com>
2025-07-04 00:54:06 +0200

    update init

--- _services/enums.py
+++ _services/enums.py
@@ -1,7 +1,7 @@
 from typing import Sequence
 
 from _repository.engine import postgres
-from _repository.repository import EventFormats, SessionTypes
+from _repository.repository import Compounds, EventFormats, SessionTypes
 from sqlalchemy.orm import Session
 
 
@@ -24,6 +24,16 @@ SESSION_TYPE_IDS: Sequence[SessionTypes] = [
     SessionTypes("Sprint Shootout"),
 ]
 
+COMPOUNDS: Sequence[Compounds] = [
+    Compounds('HARD'),
+    Compounds('MEDIUM'),
+    Compounds('SOFT'),
+    Compounds('WET'),
+    Compounds('INTERMEDIATE'),
+    Compounds('UNKNOWN'),
+    Compounds('TEST_UNKNOWN')
+]
+
 def init_event_formats():
     with Session(postgres) as s:
         s.query(EventFormats)
@@ -36,4 +46,12 @@ def init_session_types():
         s.query(SessionTypes)
         if s.query(SessionTypes).count() == 0:
             s.add_all(SESSION_TYPE_IDS)
+            s.commit()
+        
+
+def init_compounds():
+    with Session(postgres) as s:
+        s.query(Compounds)
+        if s.query(Compounds).count() == 0:
+            s.add_all(COMPOUNDS)
             s.commit()
